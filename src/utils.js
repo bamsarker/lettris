451767f47b33f7e100023a20e9f2b0c80f14c513
Sam Barker <samuel.e.barker@googlemail.com>
2018-11-25 16:11:33 +0000

    added range and delay utils

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,7 @@ export const centerGameObjects = (objects) => {
     object.anchor.setTo(0.5)
   })
 }
+
+export const range = (max) => [...Array(max).keys()]
+
+export const delay = (time) => new Promise((resolve) => setTimeout(resolve, time))
